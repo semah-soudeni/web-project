4900644fb7f88bc149ca803844939ae0590e0017
awss1236 <awsssayah@gmail.com>
2026-02-06 20:16:21 +0100

    map page first draft

---
+++ assets/js/map.js
@@ -0,0 +1,19 @@
+const sidebar = document.getElementById('sidebar')
+const p = document.getElementById('test')
+
+const openWidth = '30%'
+
+function openBar(loc){
+  p.innerText = loc
+  sidebar.style.display = 'block'
+  setTimeout(()=>{
+    sidebar.style.width = openWidth
+  }, 0)
+}
+
+function closeBar(){
+  sidebar.style.width = '0%'
+  setTimeout(()=>{
+  sidebar.style.display = 'none'
+  }, 200)
+}
