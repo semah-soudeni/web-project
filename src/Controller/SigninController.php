a6899bcb57617cbf2c5f21961368cce0f4b59b97
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-05-07 19:07:11 +0000

    add sign in and logout

--- src/Controller/SigninController.php
+++ src/Controller/SigninController.php
@@ -2,24 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Form\SigninFormType;
+use Symfony\Component\HttpFoundation\Response;
 
 final class SigninController extends AbstractController
 {
     #[Route('/signin', name: 'signin')]
-    public function index(Request $request): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        $form = $this->createForm(SigninFormType::class);
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
 
         return $this->render('signin/index.html.twig', [
             'controller_name' => 'SigninController',
             'pageTitle' => 'Sign In',
             'activePage' => 'signin',
-            'Sform' => $form->createView(),
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
         ]);
     }
 }
